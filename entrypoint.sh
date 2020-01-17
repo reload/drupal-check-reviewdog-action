11336d0f44f7ee970e34fbb3668dcb218bd9ba80
Arne Jørgensen <arne@arnested.dk>
2020-01-17 14:04:12 +0100

    Run without memory limit

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ step_log "Install latest drupal-check..."
 curl --fail --silent --output /usr/bin/drupal-check -O -L https://github.com/mglaman/drupal-check/releases/latest/download/drupal-check.phar && chmod +x /usr/bin/drupal-check
 
 step_log "Analysis check..."
-(/usr/bin/drupal-check --analysis --format=checkstyle --no-progress --no-interaction -- "${PATHS[@]}" | /usr/bin/reviewdog -f=checkstyle -name="Analysis" -reporter="${INPUT_REPORTER}") || true
+(/usr/bin/drupal-check --memory-limit=-1 --analysis --format=checkstyle --no-progress --no-interaction -- "${PATHS[@]}" | /usr/bin/reviewdog -f=checkstyle -name="Analysis" -reporter="${INPUT_REPORTER}") || true
 
 step_log "Deprecations check..."
-(/usr/bin/drupal-check --deprecations --format=checkstyle --no-progress --no-interaction -- "${PATHS[@]}" | /usr/bin/reviewdog -f=checkstyle -name="Deprecations" -reporter="${INPUT_REPORTER}") || true
+(/usr/bin/drupal-check --memory-limit=-1 --deprecations --format=checkstyle --no-progress --no-interaction -- "${PATHS[@]}" | /usr/bin/reviewdog -f=checkstyle -name="Deprecations" -reporter="${INPUT_REPORTER}") || true
