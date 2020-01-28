8cbd5945bbebd4b103638c5abb04b7b2f2169e72
Arne Jørgensen <arne@arnested.dk>
2020-01-28 09:42:18 +0100

    Don't run with pipefail
    The only place we use pipes we expect failures -- and we used to
    workaround this by using a subshell. No need for that.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -veuo pipefail
+set -veu
 
 step_log () {
     echo -e "\033[1m$*...\033[0m"
@@ -14,7 +14,7 @@ step_log "Install latest drupal-check..."
 curl --fail --silent --output /usr/bin/drupal-check -O -L https://github.com/mglaman/drupal-check/releases/latest/download/drupal-check.phar && chmod +x /usr/bin/drupal-check
 
 step_log "Analysis check..."
-(/usr/bin/drupal-check --memory-limit=-1 --analysis --format=checkstyle --no-progress --no-interaction -- "${PATHS[@]}" | /usr/bin/reviewdog -f=checkstyle -name="Analysis" -reporter="${INPUT_REPORTER}") || true
+/usr/bin/drupal-check --memory-limit=-1 --analysis --format=checkstyle --no-progress --no-interaction -- "${PATHS[@]}" | /usr/bin/reviewdog -f=checkstyle -name="Analysis" -reporter="${INPUT_REPORTER}"
 
 step_log "Deprecations check..."
-(/usr/bin/drupal-check --memory-limit=-1 --deprecations --format=checkstyle --no-progress --no-interaction -- "${PATHS[@]}" | /usr/bin/reviewdog -f=checkstyle -name="Deprecations" -reporter="${INPUT_REPORTER}") || true
+/usr/bin/drupal-check --memory-limit=-1 --deprecations --format=checkstyle --no-progress --no-interaction -- "${PATHS[@]}" | /usr/bin/reviewdog -f=checkstyle -name="Deprecations" -reporter="${INPUT_REPORTER}"
