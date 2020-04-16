2bf70e22617b9b6addde21c926a88205c659566b
Arne Jørgensen <arne@arnested.dk>
2020-04-16 22:05:00 +0200

    Uppercase first letter in report name

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,4 +16,4 @@ step_log "Install latest drupal-check..."
 [[ ! -x /usr/bin/drupal-check ]] && curl --fail --silent --output /usr/bin/drupal-check -O -L https://github.com/mglaman/drupal-check/releases/download/1.0.14/drupal-check.phar && chmod +x /usr/bin/drupal-check
 
 step_log "Check: ${INPUT_CHECK}..."
-/usr/bin/drupal-check --memory-limit=-1 "--${INPUT_CHECK}" --format=checkstyle --no-progress --no-interaction -- "${PATHS[@]}" | tee /dev/stderr | /usr/bin/reviewdog -f=checkstyle -name="${INPUT_CHECK}" -reporter="${INPUT_REPORTER}"
+/usr/bin/drupal-check --memory-limit=-1 "--${INPUT_CHECK}" --format=checkstyle --no-progress --no-interaction -- "${PATHS[@]}" | tee /dev/stderr | /usr/bin/reviewdog -f=checkstyle -name="${INPUT_CHECK^}" -reporter="${INPUT_REPORTER}"
